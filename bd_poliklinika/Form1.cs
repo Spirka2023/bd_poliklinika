d54e77475cae77f8ef122b4665d2070d2109f5f9
Bogdan S <bogdan.spiridonow2023@gmail.com>
2024-10-07 19:19:28 +0300

    Добавление всех файлов

---
+++ bd_poliklinika/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bd_poliklinika
+{
+    public partial class Form1 : Form
+    {
+        DataBase dataBase = new DataBase();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        public void LoadData() // Изменено на public
+        {
+            dataBase.openConnection();
+
+            string query = "SELECT * FROM reception";
+            SqlCommand command = new SqlCommand(query, dataBase.getConnection());
+
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+
+            dataGridView1.DataSource = table;
+
+            dataBase.closeConnection();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(this);
+            form2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(this);
+            form2.Show();
+        }
+    }
+}
---
+++ bd_poliklinika/Form2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace bd_poliklinika
+{
+    public partial class Form2 : Form
+    {
+        private Form1 form1;
+
+        public Form2(Form1 form1)
+        {
+            InitializeComponent();
+            this.form1 = form1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nameClient = textBox1.Text;
+            string nameDoctor = textBox2.Text;
+            string diagnosis = textBox3.Text;
+            string therapy = textBox4.Text;
+
+            if (!string.IsNullOrEmpty(nameClient) && !string.IsNullOrEmpty(nameDoctor) && !string.IsNullOrEmpty(diagnosis) && !string.IsNullOrEmpty(therapy))
+            {
+                DataBase dataBase = new DataBase();
+                dataBase.openConnection();
+
+                string query = "INSERT INTO reception (nameClient, nameDoctor, diagnosis, therapy) VALUES (@nameClient, @nameDoctor, @diagnosis, @therapy)";
+                SqlCommand command = new SqlCommand(query, dataBase.getConnection());
+                command.Parameters.AddWithValue("@nameClient", nameClient);
+                command.Parameters.AddWithValue("@nameDoctor", nameDoctor);
+                command.Parameters.AddWithValue("@diagnosis", diagnosis);
+                command.Parameters.AddWithValue("@therapy", therapy);
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Запись успешно добавлена!");
+                    form1.LoadData(); // Обновление данных на Form1
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при добавлении записи: " + ex.Message);
+                }
+
+                dataBase.closeConnection();
+            }
+            else
+            {
+                MessageBox.Show("Все поля должны быть заполнены!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int id;
+            if (int.TryParse(textBox5.Text, out id) && id > 0)
+            {
+                DataBase dataBase = new DataBase();
+                dataBase.openConnection();
+
+                string query = "DELETE FROM reception WHERE reception_id = @Id";
+                SqlCommand command = new SqlCommand(query, dataBase.getConnection());
+                command.Parameters.AddWithValue("@Id", id);
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Запись успешно удалена!");
+                    form1.LoadData(); // Обновление данных на Form1
+                    this.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при удалении записи: " + ex.Message);
+                }
+
+                dataBase.closeConnection();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, введите корректный ID для удаления.");
+            }
+        }
+    }
+}
